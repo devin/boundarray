9b77ca4bc881808163394788c4de355f8561c50d
Devin Naquin <dnaquin@gmail.com>
2008-11-02 19:42:52 -0600

    added building initial list. and modification through setter

--- src/boundarray.js
+++ src/boundarray.js
@@ -1,28 +1,46 @@
 /*
  * TODO documentation
  */
-var BoundArray = function (data, id, style) {
-	var i;
+var BoundArray = function(data, id, cl) {
+	var list = document.getElementById(id);
+	var i, child;
 
-	// TODO check input.
-
+	// Set settor interceptor for all existing entries in the data array.
+	// Intercept settor and update both the array and the HTML list element.
 	for (i=0; i<data.length; i++) {
-		// Intercept setter and update both the array and the HTML list element.
-		this.__defineSetter__(i, function (j) {
+		this.__defineSetter__(i, function(j) {
 			return function(val) {
-				data[j] = val;
+				var second = document.getElementById(id).childNodes[1];
 
-				// TODO modify bounded list
+				data[j] = val;
+				second.innerHTML = val;
 
 				return val;
 			};
 		}(i));
+	}
 
-		// Intercept getter to get data from the array.
+	// Set gettor interceptor for all existing entries in the data array.
+	// Intercept gettor to get data from the array.
+	for (i=0; i<data.length; i++) {
 		this.__defineGetter__(i, function(j) {
 			return function () {
 				return data[j];
 			};
 		}(i));
 	}
+
+	// Clear and build initial list.
+	while (list.childNodes[0]) {
+		list.removeChild(list.childNodes[0]);
+	}
+	for (i=0; i<data.length; i++) {
+		child = document.createElement('li');
+		if (cl) {
+			child.setAttribute('class', cl);
+		}
+		child.innerHTML = data[i];
+		list.appendChild(child);
+	}
+
 };
